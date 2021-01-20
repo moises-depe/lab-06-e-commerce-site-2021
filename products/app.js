3888381f7b99a7047f5a137cc5b419a3956f44b7
moises depena <swe.moisesdepe@gmail.com>
2021-01-20 07:03:10 -0800

    updated render, image issue

--- products/app.js
+++ products/app.js
@@ -1,11 +1,3 @@
 import { cbdProduct } from './data.js';
 import { renderProduct } from './render-cbd';
 
-
-const list = document.getElementById('list');
-
-for (let cbdProducts of cbdProducts) {
-    const cbdElementElement = renderProduct(cbdProducts);
-
-    list.prepend(cbdElementElement);
-}
