64491bce04f1494f7e5b3b3ac06fb11d4546b01d
moises depena <swe.moisesdepe@gmail.com>
2021-01-21 13:36:23 -0800

    cartAPi

--- cart/data-cart.js
+++ cart/data-cart.js
@@ -1,13 +1,21 @@
 export const cartProduct = [
 
     {
-        id: 3,
+        id: 1,
         quantity: 3
     },
     {
         id: 2,
         quantity: 1
 
+    },
+    {
+        id: 3,
+        quantity:2
+    },
+    {
+        id: 4,
+        quantity:2
     },
     {
         id: 5,
